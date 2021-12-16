f53fa9aaddf4d9ca18c052704e36e5f80dea634a
Clif Wolfe <clifwolfe1@gmail.com>
2021-12-16 10:00:39 -0500

    Made get macros much more robust, now strips multiline comments and can assign multiline comments in python without having escape character everywhere

--- scripts/get_macros.py
+++ scripts/get_macros.py
@@ -6,16 +6,29 @@ import json
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("path", help="path to a file to get macros from")
-parser.add_argument("-s", "--singleline", default=False, action="store_true",
+parser.add_argument("path", help="path to a file to get macros from",
+                    type=os.path.expanduser)
+parser.add_argument("-s", "--singleline",
+                    default=False, action="store_true",
                     help="print singleline macros")
-parser.add_argument("-m", "--multiline", default=False, action="store_true",
+parser.add_argument("-m", "--multiline",
+                    default=False, action="store_true",
                     help="print multiline macros")
-parser.add_argument("-j", "--dump-json", default=False, action="store_true",
+parser.add_argument("-j", "--dump-json",
+                    default=False, action="store_true",
                     help="dump out macros as json")
-parser.add_argument("-p", "--python-assignments", default=False, action="store_true",
-                    help="print out single line macros as assignments")
+parser.add_argument("-p", "--python-assignments",
+                    default=False, action="store_true",
+                    help="print out single line macros as assignments "
+                         "compatible with python. strips comments")
+parser.add_argument("-c", "--keep-comments",
+                    default=False, action="store_true",
+                    help="Don't strip comments from output ")
+parser.add_argument("-d", "--keep-defines",
+                    default=False, action="store_true",
+                    help="Keep in '#defines'. Ignored for python")
 args = parser.parse_args()
+
 
 # (?m)  -- inline MULTILINE FLAG
 # ^#define  -- start of string and literal values
@@ -25,24 +38,44 @@ args = parser.parse_args()
 # )*  -- 0 or more of non captured group
 # .*)$  -- the rest of the macro after '#define' if it is single line, or
 #         the rest of the last line of the macro if it is multi line
-rexp = re.compile(r'(?m)^#define ((?:.*\\\r?\n)*.*)$')
-with open(os.path.expanduser(args.path), 'r') as f:
+rexp = re.compile(r'(?m)^#define\s+((?:.*\\\r?\n)*.*)$')
+multiline_comment_regex = re.compile(r"/\*[^*]*\*+(?:[^/*][^*]*\*+)*/")
+with open(args.path, 'r') as f:
     content = f.read()
 
 
 matches = [i.groups()[0] for i in re.finditer(rexp, content)]
-matches = [re.sub(r'(\t| +)', ' ', i) for i in matches]
-split_matches = [i.split(' ', 1) for i in matches]
+# remove most of the whitespace that is present
+matches = [re.sub(r'\\*\s+', ' ', i) for i in matches]
+if args.keep_comments is False:
+    matches = [re.sub(multiline_comment_regex, '', i) for i in matches]
+
+# separate out the function like macros from the object like
+split_matches = []
+function_like = []
+for i in matches:
+    splt = i.split(' ', 1)
+    if splt[0].find('(') > -1:
+        function_like.append(i)
+    else:
+        split_matches.append(splt)
+
+# split_matches = [i.split(' ', 1) for i in matches]
 macro_map = dict([i if len(i) > 1 else i + [""] for i in split_matches])
-single_line_macro_map = {k: v for k, v in macro_map.items() if '\n' not in v}
+python_repr_macro_map = {k: v for k, v in macro_map.items() if '\n' not in v}
+
+define_string = ''
+if args.keep_defines is True:
+    define_string = '#define '
 
 singleline = []
 multiline = []
 for i in matches:
+    macrostring = define_string + i
     if '\n' in i:
-        multiline.append(i)
+        multiline.append(macrostring)
     else:
-        singleline.append(i)
+        singleline.append(macrostring)
 
 if args.singleline is True:
     for i in singleline:
@@ -57,9 +90,12 @@ if args.dump_json is True:
     print(json.dumps(macro_map, indent=2))
 
 if args.python_assignments is True:
-    for k, v in single_line_macro_map.items():
+    for k, v in python_repr_macro_map.items():
         if v == "":
             v = "None"
         print("%s = %s" % (k, v))
+    print("\n# Function like macros")
+    for i in function_like:
+        print("# %s" % i)
 
 
