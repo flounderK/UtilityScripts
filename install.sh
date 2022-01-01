504c5adc0ef060bf25c5a8193901b4d9342443fb
Clif Wolfe <clifwolfe1@gmail.com>
2022-01-01 13:10:19 -0500

    Hopefully this will start tracking branches instead of commits

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 install ./scripts/* ~/.local/bin/
 git submodule init
-git submodule update
+git submodule update --remote
 find . -mindepth 2 -type f -iname 'install.sh' | xargs -I '{}' sh -c "{}"
