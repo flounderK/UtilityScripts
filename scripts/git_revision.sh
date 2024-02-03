deaebfea1ab02b709d63a64d371b3afd0b772911
Clif Wolfe <clifwolfe1@gmail.com>
2024-02-03 11:24:24 -0500

    added note about what git_revision actuall does

--- scripts/git_revision.sh
+++ scripts/git_revision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+# This script is supposed to sync git revisions correctly for projects like chromium that have git repos that aren't git submodules, but are underneath one main repo. It tries to sync by date right now, but is still a work in progress
 COMMIT_DATE=$(git show -s --format=%ci)
 
 ORIGINAL_DIR=$(pwd)
@@ -8,9 +8,10 @@ ORIGINAL_DIR=$(pwd)
 # needed to make find work correctly with spaces in the path names.
 # Also has to be bash now
 PATHS=()
-while IFS=  read -r -d $'\0'; do
+while IFS=  read -r -d $'\0' -u 9; do
+	echo "$REPLY"
     PATHS+=("$REPLY")
-done < <(find . -mindepth 2 -type d -name '.git' -print0)
+done 9< <(find . -mindepth 2 -type d -name '.git' -print0)
 
 for DIRECTORY in "${PATHS[@]}"; do
 	DIRPATH="$(dirname "$DIRECTORY")"
@@ -20,6 +21,7 @@ for DIRECTORY in "${PATHS[@]}"; do
 	cd "$DIRPATH"
 	# TODO: might need to pick tags if present
 	COMMIT_HASH=$(git rev-list --max-count=1 --all --before="$COMMIT_DATE")
+	echo "found hash ${COMMIT_HASH}"
 	git checkout "$COMMIT_HASH"
 	# TODO: might want to do an aggressive git clean here
 	# git status
